eecb4025c00878e74086ef9b2d46963cc0f1677b
Damian Ospara <damian89m@gmail.com>
2016-12-01 01:07:49 +0100

    Basic hallo world

---
+++ generate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mvn archetype:generate -B \
+        -DarchetypeGroupId=net.alchim31.maven \
+        -DarchetypeArtifactId=scala-archetype-simple \
+        -DarchetypeVersion=1.5 \
+        -DgroupId=pl.arapso.scaffoldings \
+        -DartifactId=scala \
+        -Dversion=0.1-SNAPSHOT \
+        -Dpackage=pl.arapso
