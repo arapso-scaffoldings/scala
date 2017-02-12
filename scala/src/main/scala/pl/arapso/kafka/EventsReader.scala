b7f5f919a883aa014d32abe4d38ec82efd42fcd3
Damian Ospara <damian89m@gmail.com>
2017-02-12 13:30:00 +0100

    Not working events reader report

--- scala/src/main/scala/pl/arapso/kafka/EventsReader.scala
+++ scala/src/main/scala/pl/arapso/kafka/EventsReader.scala
@@ -5,8 +5,12 @@ import java.nio.file.Path
 import akka.actor.{Actor, ActorRef}
 
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class Register(actor: ActorRef)
+
+
 
 /**
   * Read logs line and send it do registered actors
@@ -16,16 +20,29 @@ class EventsReader extends Actor {
 
   var actors = ListBuffer[ActorRef]()
 
+  case class Tick(n: Int)
+
+  val reportDuration = 3.second
+
+  context.system.scheduler.scheduleOnce(reportDuration, self, Tick(1))
+
+  var readLines = 0
+
   override def receive: Receive = {
     case Register(actor) => {
       actors += actor
     }
     case IncomingFile(filePath: Path) => {
       scala.io.Source.fromFile(filePath.toFile).getLines().foreach(line => {
+        readLines += 1
         actors.foreach(a => {
           a ! Event(line)
         })
       })
+    }
+    case Tick(n) => {
+      println(s"[EventReader] Read lines $readLines)")
+      context.system.scheduler.scheduleOnce(reportDuration, self, Tick(n + 1))
     }
   }
 }
