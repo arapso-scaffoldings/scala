c036235e86e741d699fe344a6d2294c8bda61636
Damian Ospara <damian@roq.ad>
2017-03-31 11:23:41 +0200

    Create sbt project hierarchy

--- scala-tutor/build.sbt
+++ scala-tutor/build.sbt
@@ -1,10 +1,13 @@
-name := "ScalaTutor"
+import Dependencies._
 
-version := "1.0"
-
-libraryDependencies += "org.scala-lang" % "scala-swing" % "2.11+"
-libraryDependencies += "org.scala-lang" % "scala-library" % "2.10.6"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
-
-scalaVersion := "2.10.6"
-    
+lazy val custom = (project in file("custom")).
+  settings(
+    inThisBuild(List(
+      organization := "com.example",
+      scalaVersion := "2.11.8",
+      version      := "0.1.0-SNAPSHOT"
+    )),
+    name := "CustomExamples",
+    libraryDependencies += scalaTest % Test,
+    libraryDependencies ++= spark
+  )
---
+++ scala-tutor/project/Dependencies.scala
@@ -0,0 +1,54 @@
+import sbt._
+
+object Dependencies {
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
+
+  private[this] val akkaVersion = "2.4.17"
+  private[this] val akkaHttpVersion = "10.0.5"
+  private[this] val sparkVersion = "2.1.0"
+  private[this] val hadoopVersion = "2.7.3"
+
+  val spark = {
+    Seq(
+      "org.apache.hadoop" % "hadoop-hdfs" % hadoopVersion,
+      "org.apache.hadoop" % "hadoop-client" % hadoopVersion,
+      "org.apache.spark" %% "spark-core" % sparkVersion
+    )
+  }
+
+  val akka = {
+    Seq(
+      "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+      "com.typesafe.akka" %% "akka-agent" % akkaVersion,
+      "com.typesafe.akka" %% "akka-camel" % akkaVersion,
+      "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
+      "com.typesafe.akka" %% "akka-cluster-metrics" % akkaVersion,
+      "com.typesafe.akka" %% "akka-cluster-sharding" % akkaVersion,
+      "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
+      "com.typesafe.akka" %% "akka-contrib" % akkaVersion,
+      "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion,
+      "com.typesafe.akka" %% "akka-osgi" % akkaVersion,
+      "com.typesafe.akka" %% "akka-persistence" % akkaVersion,
+      "com.typesafe.akka" %% "akka-persistence-tck" % akkaVersion,
+      "com.typesafe.akka" %% "akka-remote" % akkaVersion,
+      "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+      "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+      "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion,
+      "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+      "com.typesafe.akka" %% "akka-distributed-data-experimental" % akkaVersion,
+      "com.typesafe.akka" %% "akka-typed-experimental" % akkaVersion,
+      "com.typesafe.akka" %% "akka-persistence-query-experimental" % akkaVersion
+    )
+  }
+
+  val akkaHttp = {
+    Seq(
+      "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-jackson" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-xml" % akkaHttpVersion
+    )
+  } ++ akka
+}
