96c24b46b61f441cac760304a2043ad6d5633d84
Damian Ospara <damian89m@gmail.com>
2017-04-21 00:24:06 +0200

    Add simple client request and return response

--- scala-tutor/akka/src/main/scala/pl/arapso/scaffoldings/akka/SwApiProxy.scala
+++ scala-tutor/akka/src/main/scala/pl/arapso/scaffoldings/akka/SwApiProxy.scala
@@ -2,25 +2,33 @@ package pl.arapso.scaffoldings.akka
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 
 /**
-  * Simple example how tu create rest api proxy wuth akka akka-http.
+  * Simple example how tu create rest api proxy with akka akka-http.
   */
 object SwApiProxy extends App {
   implicit val system = ActorSystem()
-  implicit val materlialize = ActorMaterializer()
+  implicit val materlializer = ActorMaterializer()
+
   import system.dispatcher
 
+  val param1 = parameter('param1)
+  val param2 = parameter('param2)
 
-  val sw_get = path("/hello") & get & parameters("param1", "param2")
+  val helloParameters = param1 & param2
 
-  val route = sw_get { (param1: String, param2: String) =>
-    complete(s"OK ${param1} ${param2}")
+  val sw_get = get & path("luke")
+
+  val route: Route = sw_get {
+    val response = Http().singleRequest(HttpRequest(uri = "https://swapi.co/people/1"))
+    complete(response.map(_.entity.toString))
   }
 
+  // TODO print on console basic binding events http bind end etc
   Http().bindAndHandle(route, "localhost", 8080)
-
 
 }
